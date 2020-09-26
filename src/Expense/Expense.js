66e655e1c89aef98e14138267f9cffa62c85afe7
Aidan McKay <aidanmckay2000@gmail.com>
2020-09-26 10:19:23 -0500

    Add basic delete functionality for expenses

--- src/Expense/Expense.js
+++ src/Expense/Expense.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export const Expense = ({ amount, date }) => {
+export const Expense = ({ amount, date, deleteExpense, id }) => {
   return (
     <article>
       <h2>{amount}</h2>
       <h3>{date}</h3>
+      <button onClick={() => deleteExpense(id)}>Delete</button>
     </article>
   )
 }
