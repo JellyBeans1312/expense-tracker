f6d1555bb174c7696f18ce8aae614415099a9d93
Aidan McKay <aidanmckay2000@gmail.com>
2020-09-26 10:17:55 -0500

    Add basic delete functionanlity for an account

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const Account = ({ title, color }) => {
+export const Account = ({ title, color, id, deleteAccount }) => {
   return (
     <article>
       <h2>{title}</h2>
+      <button onClick={() => deleteAccount(id)}>Delete Account</button>
     </article>
   )
 }
--- src/AccountContainer/AccountContainer.js
+++ src/AccountContainer/AccountContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Account } from '../Account/Account'
 
-export const AccountContainer = ({ accounts }) => {
+export const AccountContainer = ({ accounts, deleteAccount }) => {
   const account = accounts.map(account => {
-    return <Account title={account.title} color={account.date} icon={account.icon} key={account.id} />
+    return <Account title={account.title} color={account.date} icon={account.icon} key={account.id} id={account.id} deleteAccount={deleteAccount} />
   })
   return account
 }
